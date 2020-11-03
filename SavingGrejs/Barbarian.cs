a21237796cf76a549113f9e181e944139035c0dc
Eskilll <eskil.nordlander@elev.ga.ntig.se>
2020-11-03 13:16:41 +0100

    Final commit

---
+++ SavingGrejs/Barbarian.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SavingGrejs
+{
+    public class Barbarian
+    {
+        public float Hp;
+        public float damage;
+        public string name;
+        
+    }
+}
--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 namespace SavingGrejs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ans = 0;
+            bool wot = false;
+            Barbarian theBarbarian = new Barbarian();
+
+            XmlSerializer barbSerializer = new XmlSerializer(typeof(Barbarian));
+                        //hp
+            while(wot != true ){
+                    System.Console.WriteLine("Hp...");
+                    string tal = System.Console.ReadLine();
+                    wot = int.TryParse(tal, out ans);
+                    if(wot == false){
+                        System.Console.WriteLine("NUMBERS YOU FUK");
+                    }
+                    theBarbarian.Hp = ans;
+                    }
+
+                    //name
+                    System.Console.WriteLine("Choose name FOOL");
+                    string name = System.Console.ReadLine();
+                    theBarbarian.name = name;
+
+                    
+                    //damage
+            while(wot != true ){
+                    System.Console.WriteLine("Choose how much damage it does!");
+                    string tal = System.Console.ReadLine();
+                    wot = int.TryParse(tal, out ans);
+                    if(wot == false){
+                        System.Console.WriteLine("NUMBERS YOU FUK");
+                    }
+                    theBarbarian.damage = ans;
+                    }
+            
+            using (FileStream file = File.Open(@"barb.xml", FileMode.OpenOrCreate))
+                {
+                    barbSerializer.Serialize(file, theBarbarian);
+                }
+                    
+                    
+                   
         }
     }
 }
